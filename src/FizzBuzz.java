9816b3d7fa7b79f9855cf6ff79af126b3f16d084
sheba <sheba8367@gmail.com>
2022-09-21 09:30:30 -0700

    stuff

---
+++ src/FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class FizzBuzz {
+    //Given an integer n, return a string array answer (1-indexed) where:
+
+    //answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    //answer[i] == "Fizz" if i is divisible by 3.
+    //answer[i] == "Buzz" if i is divisible by 5.
+    //answer[i] == i (as a string) if none of the above conditions are true.
+    public static void main(String[] args) {
+        System.out.print(fizzBuzz(100, 2, 7));
+    }
+
+    private static List<String> fizzBuzz(int n,int o,int p) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i%(o * p) == 0) {
+                list.add("FizzBuzz");
+            }
+            else if (i%o == 0) {
+                list.add("Buzz");
+            }
+            else if (i%p == 0){
+                list.add("Fizz");
+            }
+            else {
+                list.add(Integer.toString(i));
+            }
+        }
+        return list;
+    }
+}
